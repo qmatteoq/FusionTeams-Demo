3cbeef2c48bc4898ec0ef57741a8d5f80d29c033
Matteo Pagani <matteo.pagani@microsoft.com>
2022-12-08 19:32:11 +0100

    Added customers minimal API

---
+++ Customers/CustomersMinimalApi/Program.cs
@@ -0,0 +1,29 @@
+using CustomersServices.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ICustomerService, CustomerService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/customers", (HttpContext context, ICustomerService customerService) => {
+    var customers = customerService.GetCustomers();
+    return customers;
+})
+.WithName("GetCustomers")
+.WithOpenApi();
+
+app.Run();
